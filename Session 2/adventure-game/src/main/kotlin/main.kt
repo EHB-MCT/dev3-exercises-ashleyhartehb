91375615a61d53bfd161ce7755a96c260057f056
Ashley J. Hart <ashley.hart@student.ehb.be>
2021-10-12 11:44:42 +0200

    issue with project and folders
    there was a adventure-game folder outside session 2 and the project I was working on was in session 2, I moved something and nothing worked anymore

--- Session 2/adventure-game/src/main/kotlin/main.kt
+++ Session 2/adventure-game/src/main/kotlin/main.kt
@@ -1,13 +1,13 @@
 var tryCount = 1;
 
-fun main(){
+fun main() {
     println("You are in a post apocalyptic world where machines roam the lands and some of them are corrupted by a certain cult. You're an outcast of your tribe because you've been born without parents and someone of the tribe chose to raise you instead. You're a very curious person who likes to learn more about the machines and their history.")
     println("You see an ancient door with 4 round slots.")
     challengeOne()
 }
 
-fun challengeOne(){
-    val numbers = listOf("VI","II","V","I")
+fun challengeOne() {
+    val numbers = listOf("VI", "II", "V", "I")
 
     println("Choose 4 roman numbers for each slot and press enter after each number")
 
@@ -15,25 +15,27 @@ fun challengeOne(){
     val slot2 = readLine()
     val slot3 = readLine()
     val slot4 = readLine()
-    val userNumbers = listOf(slot1,slot2,slot3,slot4)
+    val userNumbers = listOf(slot1, slot2, slot3, slot4)
 
-    if(numbers==userNumbers){
+    if (numbers == userNumbers) {
         success()
-    }else{
+    } else {
         gameOver()
     }
 }
 
-fun success(){
-println("Congratulations you found the answer on try n° ${tryCount}!")
+
+
+fun success() {
+    println("Congratulations you won the game!")
 }
 
-fun gameOver(){
+fun gameOver() {
     println("Wrong answer")
     val hint1 = "Your device shows the following numbers: I V II VI "
     val hint2 = "It looks like the pannel has been turned downwards"
     tryCount++
-    when (tryCount){
+    when (tryCount) {
         2 -> println(hint1)
         3 -> println(hint2)
         else -> {
